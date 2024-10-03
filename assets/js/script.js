45445ba8473a64ca3935082d680f4217d1a336ae
amirAghayari <amiraghayari2119@gmail.com>
2024-10-03 20:16:58 +0330

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@
 
 const word = document.querySelector(".searchInput");
 const addBtn = document.querySelector(".searchBtn");
+const form = document.querySelector(".searchForm");
 const wordComponent = document.getElementById("wordContainer");
 
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-addBtn.addEventListener("click", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevent form from submitting normally
   let inputValue = word.value.trim();
   wordComponent.style.opacity = "1";
 
@@ -16,17 +18,16 @@ addBtn.addEventListener("click", () => {
       wordComponent.innerHTML = `
       <div class="wordDiv">
         <h1>${inputValue}</h1>
-        <button type="button">
-          <i class="fas fa-volume-up">${data[0].phonetics[0].audio}</i>
-        </button>
+    
       </div>
       
       <p class="pronoun">${data[0].phonetics[1].text}</p>
       
-      <p class="definition>
-      ${data[0].meanings[2].definitions[1].definition}</p>      
-      
-      `;
-      console.log(data);
+      <h2 class="definition">
+        ${data[0].meanings[1].definitions[0].definition}
+      </h2>
+
+
+`;
     });
 });
